8176458d6b68034f10bf84c28f51b923dfc6c52f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-01 12:14:59 +0100

    Added comments

--- python_sub.py
+++ python_sub.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+# Demonstrační příklady využívající systém Redis
+
+
 import redis
 import time
 
@@ -6,10 +12,12 @@ CHANNEL_NAME = "kanal1"
 
 
 def connect(host, port):
+    """Připojení k Redisu na specifikované adrese a portu."""
     return redis.Redis(host=host, port=port)
 
 
 def sub(host, port, channel):
+    """Operace typu SUB - příjem zpráv z Redisu."""
     r = connect(host, port)
     pubsub = r.pubsub()
     pubsub.subscribe(channel)
