b0d96c5006f7e3403e8750cbfac9fcb499a78244
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-01 12:13:28 +0100

    Added comments

--- python_pub_faster.py
+++ python_pub_faster.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+# Demonstrační příklady využívající systém Redis
+
+
 import redis
 import time
 
@@ -6,12 +12,15 @@ CHANNEL_NAME = "kanal1"
 
 
 def connect(host, port):
+    """Připojení k Redisu na specifikované adrese a portu."""
     return redis.Redis(host=host, port=port)
 
 
 def pub(host, port, channel):
+    """Operace typu PUB - publikace zpráv do Redisu."""
     r = connect(host, port)
 
+    # budeme publikovat deset zpráv
     for i in range(0, 21):
         print("Publishing message to " + channel)
         message = "zprava #{}".format(i)
