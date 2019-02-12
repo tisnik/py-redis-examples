4b271107c737b917bfe8f4bc9e5c770cb54dfe57
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-12 14:07:08 +0100

    Added license to all source files

--- python_sub.py
+++ python_sub.py
@@ -3,6 +3,18 @@
 
 # Demonstrační příklady využívající systém Redis
 
+#
+#  (C) Copyright 2018, 2019  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 
 import redis
 import time
