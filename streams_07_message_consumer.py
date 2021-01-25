7677467692241cd167fe92c90faf6ebbb0392caf
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-25 09:09:30 +0100

    Redis streams example #2: simple benchmark - message consumer

---
+++ streams_07_message_consumer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import time
+
+from walrus import Database
+
+db = Database()
+stream = db.Stream("streamY")
+
+counter = 0
+
+start = time.time()
+
+last_id = "0"
+
+while True:
+    messages = stream.read(block=0, last_id=last_id, count=1)
+    message = messages[0]
+    last_id = message[0]
+    content = message[1]
+    counter += 1
+    if b"last" in content and content[b"last"] == b"y":
+        break
+
+end = time.time()
+print("Consumer duration for {} messages: {} seconds".format(counter, (end - start)))
