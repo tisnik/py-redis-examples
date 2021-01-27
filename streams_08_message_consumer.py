30046afd07f7940cb6fcfb21bdd7e2b57f3f5275
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-27 09:18:06 +0100

    Message consumer

---
+++ streams_08_message_consumer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import time
+
+from walrus import Database
+
+db = Database()
+stream = db.Stream("streamY")
+
+start = time.time()
+
+messages = stream.read(last_id=0, count=100000)
+counter = len(messages)
+
+end = time.time()
+print("Consumer duration for {} messages: {} seconds".format(counter, (end - start)))
