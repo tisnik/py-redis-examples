aa37b84da1e218d3aec7f4d13ed62ee83b643f72
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-27 09:18:26 +0100

    Usage of consumer group

---
+++ streams_10_read_new_messages_group.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from walrus import Database
+
+db = Database()
+
+cg = db.consumer_group("a-group", ["streamX"])
+cg.create()
+cg.set_id('$')
+
+while True:
+    messages = cg.read(block=0)
+    for message in messages:
+        print(message)
